abe5d7b0483fffbe9cb658a48ee45d7effd07591
Patea Tlau <pateatlau@gmail.com>
2025-04-04 10:13:59 +0530

    express and dotenv added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello Typescript!!!');
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const HELLO = 'Hello Typescript NodeJS!!!';
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req: Request, res: Response) => {
+  console.log('GET Request received in / route');
+  res.send(HELLO);
+});
+
+console.log(HELLO);
