ac4a8090ba3868b21d90c3b9e9c022003de351aa
Patea Tlau <pateatlau@gmail.com>
2025-04-06 13:29:51 +0530

    Basic http server config updated for serving API's and authentication.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
-import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express, { Request, Response } from 'express';
+import http from 'http';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(cors({ credentials: true}));
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const server = http.createServer(app);
+server.timeout = 0; // Disable timeout
+server.keepAliveTimeout = 0; // Disable keep-alive timeout
+server.headersTimeout = 0; // Disable headers timeout
+server.setTimeout(0); // Disable server timeout
+
 const HELLO = 'Hello Typescript NodeJS!!!';
 
 app.listen(PORT, () => {
